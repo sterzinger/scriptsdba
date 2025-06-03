103ddb74ac59c7b2b72f7d4f9409b3d09b5e40f3
sterzinger <82913197+sterzinger@users.noreply.github.com>
2025-06-03 07:53:28 -0300

    Add files via upload

--- login.sql
+++ login.sql
@@ -1,3 +1,12 @@
-set pagesize 300 linesize 400
-col report format a400
-def _editor=vi
+## Login prompt for SQL*PLUS connection
+
+column global_name new_value gname
+set termout off
+select lower(user) || '@' ||
+decode(global_name, 'ORACLE8.WORLD', '8.0', 'ORA8I.WORLD',
+'8i', 'ORA8IDEV.WORLD', 'dev8i', global_name ) global_name from
+global_name;
+set termout on
+set pages 120
+set lines 120
+set sqlprompt '&gname> '
